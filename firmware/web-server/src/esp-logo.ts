8e932d764931251585eaea0c551cf3c8be9c1247
Florian <florian.hess@azon.ai>
2025-10-20 02:14:38 +0200

    CI: Build webserver bundle

---
+++ firmware/web-server/src/esp-logo.ts
@@ -0,0 +1,17 @@
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators.js";
+
+import logoUrl from "/logo.png?url";
+
+@customElement("esp-logo")
+export default class EspLogo extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+
+  render() {
+    return html`<img src="${logoUrl}" alt="ESPHome Logo" style="width: 40px; height: 40px;" />`;
+  }
+}
+
+
